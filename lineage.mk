5fd29a5679a4be1330f603f4b5657affed64c1b3
Blue Dogerino <fsalamic16@gmail.com>
2018-03-28 19:27:29 +0000

    fix lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -4,10 +4,10 @@ $(call inherit-product, device/samsung/j5x-common/lineage.mk)
 $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := o7prolte
-PRODUCT_NAME := lineage_o7prolte
+PRODUCT_DEVICE := j5xnlte
+PRODUCT_NAME := lineage_j5xnlte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G600FY
+PRODUCT_MODEL := SM-J510FN
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
 
