305efaa41be02e9ecd06ec948852825d5721b989
Blue Dogerino <fsalamic16@gmail.com>
2018-04-02 20:22:33 +0200

    add firehound stuff

--- fh.mk
+++ fh.mk
@@ -1,14 +1,13 @@
-
 # Inherit from common
-$(call inherit-product, device/samsung/j5x-common/lineage.mk)
+$(call inherit-product, device/samsung/j5x-common/fh.mk)
 
 $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5xnlte
-PRODUCT_NAME := lineage_j5xnlte
+PRODUCT_NAME := fh_j5xnlte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-J510F
+PRODUCT_MODEL := SM-J510FN
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
 
--- omni_j5xnlte.mk
+++ omni_j5xnlte.mk
@@ -5,9 +5,9 @@ $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5xnlte
-PRODUCT_NAME := omni_j5xnlte
+PRODUCT_NAME := omni_j5lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-J510FN
+PRODUCT_MODEL := SM-J510F
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
 
